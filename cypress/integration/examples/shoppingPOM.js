5dafe823f5cfdb0a0aeb9d6867b3789083b89e8d
Demmy <demolaabdulai@live.com>
2021-04-22 09:33:43 +0200

    made the const globa;

--- cypress/integration/examples/shoppingPOM.js
+++ cypress/integration/examples/shoppingPOM.js
@@ -2,6 +2,9 @@
 import HomePage from '../examples/pageObject/homePage'
 import ProductPage from '../examples/pageObject/productPage'
 
+const homePage = new HomePage()
+const productPage = new ProductPage()
+
 describe('Framework Test', function() {
     beforeEach(() => {
         // runs before each test in the block
@@ -10,7 +13,6 @@ describe('Framework Test', function() {
         })
     })
     it('should fill the form and assert them', function() {
-        const homePage = new HomePage()
         cy.visit('https://rahulshettyacademy.com/angularpractice')
         homePage.getEditBox().type(this.data.name)
         homePage.getGender().select(this.data.gender)
@@ -20,17 +22,15 @@ describe('Framework Test', function() {
     })
 
     it('should click on the shop menu and select the items ', function() {
-        const homePage = new HomePage()
-        const productPage = new ProductPage()
+        Cypress.config("defaultCommandTimeout", 8000)
         homePage.clickShopButton().click()
         this.data.productName.forEach(function(element) {
-                cy.selectProduct(element)
-            })
-            //used if you create a function in command.js and call it here 
-            //cy.selectProduct('Nokia Edge')            
-            //cy.selectProduct('Blackberry')
-
+            cy.selectProduct(element)
+        })
         productPage.clickCheckoutButton().click()
+        cy.contains('Checkout').click()
+        cy.get('#country').type('India')
+        cy.get('.suggestions > ul > li > a').click()
 
 
     })
